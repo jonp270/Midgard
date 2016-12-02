f1d06d914b04e7a149d448feeb3828d8f5c300c1
Braveheron <magjwp@ymail.com>
2016-12-01 19:08:50 -0800

    Created classes Fighter, Mage, Theif

--- Midgard/Midgard/character.cpp
+++ Midgard/Midgard/character.cpp
@@ -4,5 +4,38 @@
 
 fighter::fighter()
 {
+	str = 12;
+	dex = 10;
+	hp = 20;
+	mp = 5;
+	intelligence = 8;
+	wis = 8;
+	def = 5;
+	res = 2;
+
+}
+
+mage::mage()
+{
+	str = 8;
+	dex = 8;
+	hp = 15;
+	mp = 10;
+	intelligence = 12;
+	wis = 10;
+	def = 2;
+	res = 5;
+
+}
+
+theif::theif()
+{
 	str = 10;
+	dex = 12;
+	hp = 25;
+	mp = 5;
+	intelligence = 8;
+	wis = 8;
+	def = 4;
+	res = 3;
 }
